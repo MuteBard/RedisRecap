4bde80f3d68c0b73e2a4a786ed0eb3d039956425
MuteBard <csevere@brandeis.edu>
2019-04-17 16:46:46 -0400

    render contents from redis

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const redis = require('redis');
 
 const app = express();
 
+
+//Create Client
+var client = redis.createClient();
+client.on('connect', function(){
+    console.log('Redis Server Connected')
+})
+
 //view engine setup
 app.set('views', path.join(__dirname, 'views')); //hold all our templates and views
 app.set('view engine', 'ejs'); //use ejs as our templating engine
@@ -16,9 +23,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public'))); //set up to be our client folder
 
 app.get('/', function(req, res){
-    res.send('Welcome')
-})
+    var title = 'Games List'
 
+    client.lrange('games', 0, -1, function(err, reply){
+        res.render('index', {
+            title: title,
+            games: reply
+        });
+    })
+
+
+}) 
 
 
 app.listen(3000);
